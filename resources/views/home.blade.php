c2433abb75c92a776356ec810e8265843503017b
Eliana-Abate <eliana.abate@outlook.it>
2021-10-07 15:51:20 +0200

    added files home+main+header blade.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('content')
+
+    <h3>Benvenuti!</h3>
+    
+@endsection
---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,7 @@
+<header>
+    <h1>MOVIES</h1>
+    <nav>
+        <a href="{{route('home')}}">Home</a>
+        <a href="#">Movies</a>
+    </nav>
+</header>
